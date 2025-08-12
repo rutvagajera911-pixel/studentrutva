db81285ebfc68c3d35f784ab4b2d15e01cfedd13
rutvagajera911-pixel <rutvagajera911@gmail.com>
2025-08-12 10:15:06 +0530

    Add files via upload

---
+++ subtract two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int num1,num2,sub;
+printf("enter first number");
+scanf("%d",&num1);
+printf("enter second number");
+scanf("%d",num2);
+sub=num1+num2;
+printf("the sum of %d and %d is %d\n",num1,num2,sub);
+return 0;
+}
