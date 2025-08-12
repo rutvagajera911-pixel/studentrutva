a551bf663c2e92ef79dcf02eb862346782c454c2
rutvagajera911-pixel <rutvagajera911@gmail.com>
2025-08-12 10:29:14 +0530

    Add files via upload

---
+++ 4.devide two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int num1,num2,quotient;
+printf("enter first number");
+scanf("%d",&num1);
+printf("enter second number");
+scanf("%d",num2);
+quotient=num1/num2;
+printf("the quotient of %d and %d is %d\n",num1,num2,quotient);
+return 0;
+}
