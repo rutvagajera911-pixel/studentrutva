ed163543407ffd200426e8b5a27ad7fce73dba0b
rutvagajera911-pixel <rutvagajera911@gmail.com>
2025-08-12 10:26:15 +0530

    Add files via upload

---
+++ 3.multiply two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int num1,num2,product;
+printf("enter first number");
+scanf("%d",&num1);
+printf("enter second number");
+scanf("%d",num2);
+product=num1*num2;
+printf("the product of %d and %d is %d\n",num1,num2,product);
+return 0;
+}
