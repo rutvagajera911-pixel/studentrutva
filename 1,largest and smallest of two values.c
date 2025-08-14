fd50c1b889025618cc29bd1c7933e0b4ea5956f4
rutvagajera911-pixel <rutvagajera911@gmail.com>
2025-08-14 23:08:00 +0530

    Add files via upload

---
+++ 1,largest and smallest of two values.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf("Largest: %d\n", a);
+        printf("Smallest: %d\n", b);
+    } else if (b > a) {
+        printf("Largest: %d\n", b);
+        printf("Smallest: %d\n", a);
+    } else {
+        printf("Both numbers are equal: %d\n", a);
+    }
+
+    return 0;
+}
